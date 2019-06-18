838acaa2e31812dfec8e430ada87daf756c9ad52
abatisse <abatisse@hevaweb.com>
2019-06-18 15:44:35 +0200

    :lipstick: add UI components waiting for styling

--- apps/context.py
+++ apps/context.py
@@ -1,3 +1,4 @@
+import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
@@ -25,5 +26,47 @@ layout = html.Div(
             | col 2 is | centered | $12 |
             | col 3 is | *right-aligned* |**$1** |"""
         ),
+        html.H3(["Components"]),
+        html.H4(["Dropdown"]),
+        dcc.Dropdown(
+            options=[
+                {"label": "New York City", "value": "NYC"},
+                {"label": "Montréal", "value": "MTL"},
+                {"label": "San Francisco", "value": "SF"},
+            ],
+            value="MTL",
+        ),
+        html.H4(["Slider"]),
+        dcc.Slider(min=-5, max=10, step=0.5, value=-3),
+        html.H4(["Range Slider"]),
+        dcc.RangeSlider(count=1, min=-5, max=10, step=0.5, value=[-3, 7]),
+        html.H4(["Simple Input"]),
+        dcc.Input(placeholder="Enter a value...", type="text", value=""),
+        html.H4(["Textarea"]),
+        dcc.Textarea(
+            placeholder="Enter a value...",
+            value="This is a TextArea component",
+            style={"width": "100%"},
+        ),
+        html.H4(["Checklist"]),
+        dcc.Checklist(
+            options=[
+                {"label": "New York City", "value": "NYC"},
+                {"label": "Montréal", "value": "MTL"},
+                {"label": "San Francisco", "value": "SF"},
+            ],
+            values=["MTL", "SF"],
+        ),
+        html.H4(["Radio items"]),
+        dcc.RadioItems(
+            options=[
+                {"label": "New York City", "value": "NYC"},
+                {"label": "Montréal", "value": "MTL"},
+                {"label": "San Francisco", "value": "SF"},
+            ],
+            value="MTL",
+        ),
+        html.H4(["Button"]),
+        html.Button("Submit", id="button"),
     ]
 )

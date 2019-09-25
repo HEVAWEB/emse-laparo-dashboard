d2e3c2451fec007b95c79e7278141e4500a7c627
abatisse <abatisse@hevaweb.com>
2019-09-25 14:34:10 +0200

    :bug: fix url in eula page

--- apps/eula.py
+++ apps/eula.py
@@ -1,15 +1,11 @@
-from pathlib import Path
-
 import dash_html_components as html
 
 import utils
 
-DIR_PATH = Path(__file__).parents[1].parts[-1]
+SITE = "https://dashboard-template.hevaweb.com"
 
 with open("assets/eula/eula-fr.md", "r", encoding="utf-8") as f:
-    content = utils.MarkdownReader(
-        f.read().format(site=f"https://{DIR_PATH}.hevaweb.com")
-    )
+    content = utils.MarkdownReader(f.read().format(site=SITE))
 
 with open("assets/eula/eula-fr-content.md", "r", encoding="utf-8") as f:
     sources = f.read()

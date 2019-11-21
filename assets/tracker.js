9920b4da0418a3814235144d7b00e5070157bcd4
Jordane JOUFFROY <JJOUFFROY@hevaweb.com>
2019-11-21 14:32:58 +0100

    fix analytics

--- assets/tracker.js
+++ assets/tracker.js
@@ -1,3 +1,8 @@
-window.ga = window.ga | |function(){(ga.q=ga.q | |[]).push(arguments)}; ga.l = +new Date;
-ga('create', 'UA-75404337-15', 'auto');
-ga('send', 'pageview');
+window.ga =
+  window.ga ||
+  function() {
+    (ga.q = ga.q || []).push(arguments);
+  };
+ga.l = +new Date();
+ga("create", "UA-75404337-15", "auto");
+ga("send", "pageview");

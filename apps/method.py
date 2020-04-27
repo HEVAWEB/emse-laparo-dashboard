3d4eb44a611d81fe7251119013dffce94ae14cbe
abatisse <abatisse@hevaweb.com>
2020-04-27 11:45:07 +0200

    :pencil: fix paths

--- apps/method.py
+++ apps/method.py
@@ -3,7 +3,7 @@ import dash_html_components as html
 import utils
 
 
-with open("assets\contents\methods.md", "r", encoding="utf-8") as f:
+with open("assets/contents/methods.md", "r", encoding="utf-8") as f:
     content = utils.markdown_content(f.read())
 
 layout = html.Div([content])

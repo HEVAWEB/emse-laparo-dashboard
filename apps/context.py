e5a936252b23a2491bab33579c11b197cb7445ef
Hugo De Oliveira <HDEOLIVEIRA@hevaweb.com>
2020-09-11 17:55:42 +0200

    links

--- apps/context.py
+++ apps/context.py
@@ -7,4 +7,27 @@ with open("assets/contents/md_context.md", "r", encoding="utf-8") as f:
     content = utils.MarkdownReader(f.read())
 
 # Define the page's content
-layout = html.Div([content[0], content[1]])
+layout = html.Div(
+    [
+        content[0],
+        html.Ul(
+            [
+                html.Li(
+                    html.A(
+                        html.Em("IEEE Xplore"),
+                        href="https://ieeexplore.ieee.org/document/9186773",
+                        target="_blank",
+                    )
+                ),
+                html.Li(
+                    html.A(
+                        html.Em("ResearchGate"),
+                        href="https://www.researchgate.net/publication/344127383_Automatic_and_Explainable_Labeling_of_Medical_Event_Logs_with_Autoencoding",
+                        target="_blank",
+                    )
+                ),
+            ]
+        ),
+        content[1],
+    ]
+)
--- assets/contents/md_context.md
+++ assets/contents/md_context.md
@@ -3,8 +3,8 @@ This dashboard is as an interactive supplementary material related to the follow
 H. De Oliveira, V. Augusto, B. Jouaneton, L. Lamarsalle, M. Prodel and X. Xie, **"Automatic and Explainable Labeling of Medical Event Logs with Autoencoding"**, in *IEEE Journal of Biomedical and Health Informatics*, doi: 10.1109/JBHI.2020.3021790.
 
 Links to the paper:
-- [*IEEE Xplore*] (https://ieeexplore.ieee.org/document/9186773)
-- [*ResearchGate*] (https://www.researchgate.net/publication/344127383_Automatic_and_Explainable_Labeling_of_Medical_Event_Logs_with_Autoencoding)
+
+[//]: # (section)
 
 The rest of this dashboard is organized as follows:
 - The __Experiments__ section presents the results obtained on synthetic data (clustering and explanation performances);
